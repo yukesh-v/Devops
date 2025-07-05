65beff402ece5572b0397f4c43debc1cfa6f3e48
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2025-07-05 21:34:45 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-05ffe3c48a9991133"
   instance_type = "t2.micro"
-  
+  key_name = "id_pair"
   tags = {
     Name = "Test1"
   }
