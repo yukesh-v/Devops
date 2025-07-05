65b84e0c73c09264dff2cdea130ca99da62c7f68
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2025-07-06 00:28:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,15 +7,23 @@ variable "instance_name" {
   description = "Name for the EC2 instance"
   type        = string
 }
+variable "tags" {
+  description = "A map of tags to apply to all resources."
+  type        = map(string)
+  default     = {} # Provide an empty map as default if not always needed
+}
 provider "aws" {
-  region = "var.region"
+  region = var.region
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-05ffe3c48a9991133"
   instance_type = "t2.micro"
   
-  tags = {
-    Name = "var.instance_name.Name"
-  }
+  tags = merge(
+    var.tags,
+    {
+      Name        = var.instance_name
+    }
+  )
 }
