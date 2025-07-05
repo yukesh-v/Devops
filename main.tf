b4872ad1685c882093210c5a9e673387128c57d9
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2025-07-06 01:52:35 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,20 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to apply to all resources."
+  type        = map(string)
+  default     = {} # Provide an empty map as default if not always needed
+}
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-05ffe3c48a9991133"
   instance_type = "t2.micro"
-  key_name = "id_pair"
-  tags = {
-    Name = "Test2"
-  }
+  
+  tags = var.tags   
 }
