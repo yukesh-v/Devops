75a4be80fdaef627f8612cb034b1294bcbe43541
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2025-07-06 18:25:29 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -8,6 +8,6 @@ variable "tags" {
   description = "A map of tags to apply to all resources."
   type        = map(string)
   default     = {
-   "Name" : "DVS-TEST1" 
+   "Name" = "DVS-TEST1" 
   } # Provide an empty map as default if not always needed
 }
