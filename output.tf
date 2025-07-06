ac031b2a7d8bc719c0c2f20e23a339b50bb75c9f
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2025-07-06 19:03:04 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "public_ip" {
- value       = aws_instance.public_instance.public_ip
+ value       = aws_instance.web.public_ip
  description = "Public IP Address of EC2 instance"
 }
 
 output "instance_id" {
- value       = aws_instance.public_instance.id
+ value       = aws_instance.web.id
  description = "Instance ID"
 }
