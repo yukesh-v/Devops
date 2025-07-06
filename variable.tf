547ad9268b080d2c027fd26d834b91adb0323937
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2025-07-06 18:40:55 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,10 @@
 variable "region" {
   description = "AWS Region"
   type        = string
-default     = ""
 }
 
 variable "tags" {
   description = "A map of tags to apply to all resources."
   type        = map(string)
-  default     = {
-   "Name" = "" 
-  } 
+  default     = {} 
 }
