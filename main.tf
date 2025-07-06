65dc8af7d5d9932550b264ba33c3a81d3c897b45
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2025-07-06 18:18:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
-variable "region" {
-  description = "AWS Region"
-  type        = string
-}
 
-variable "tags" {
-  description = "A map of tags to apply to all resources."
-  type        = map(string)
-  default     = {} # Provide an empty map as default if not always needed
-}
 provider "aws" {
   region = var.region
 }
