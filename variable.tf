8554275b4a07ed5639208a4dfebb0eeba4240d0e
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2025-07-06 18:25:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,5 +7,7 @@ default     = "us-east-1"
 variable "tags" {
   description = "A map of tags to apply to all resources."
   type        = map(string)
-  default     = {Name : DVS-TEST1} # Provide an empty map as default if not always needed
+  default     = {
+   "Name" : "DVS-TEST1" 
+  } # Provide an empty map as default if not always needed
 }
