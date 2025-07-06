8e06c54bccb1d41f60b0ed6265cc90e1fd2942af
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2025-07-06 18:22:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,11 @@
 variable "region" {
   description = "AWS Region"
   type        = string
+default     = "us-east-1"
 }
 
 variable "tags" {
   description = "A map of tags to apply to all resources."
   type        = map(string)
-  default     = {} # Provide an empty map as default if not always needed
+  default     = {Name : DVS-TEST1} # Provide an empty map as default if not always needed
 }
