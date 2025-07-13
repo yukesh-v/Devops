af8a414043b0d82880fc8689623f5c0d7353594f
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2025-07-13 20:31:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-05ffe3c48a9991133"
   instance_type = "t2.micro"
-  key_name ="id_pair"
+  key_name ="ansible_key"
   tags = var.tags   
 }
